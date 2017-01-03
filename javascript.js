993e2706c00eeed7cc0fe8d4a109b63e8c1c8975
Thorin Butler <thorin.butler@gmail.com>
2017-01-02 19:45:16 -0600

    Added the ability to change the background color of the button to a limited selection of colors.  Not much of a commit, but it's me doing some coding for the day.  Babysteps.  There are better ways to do this I'm sure.  I'm sure I've even done some of them, but still.  I was able to look at some code, sort out what it did, and warp it to my whims a bit.  I'll put that in the win column.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
 var para = document.querySelector('p');
+var style = document.querySelector('style');
 
 para.addEventListener('click', updateName);
+
+para.addEventListener('click', updateStyle)
 
 function updateName() {
   var name = prompt('Enter a new name');
   para.textContent = 'Player 1: ' + name;
+}
+
+function updateStyle() {
+  var color = prompt("red, blue, or yellow?");
+  style.textContent = 'p {color: black; background:' + color + '}';
 }
