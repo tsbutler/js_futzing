c5bbab81c624421a3ed1a88181d3fd676dbdf793
Thorin Butler <thorin.butler@gmail.com>
2017-01-03 21:27:26 -0600

    Trying to see what other changes I can make.  Not terribly complex, but any practice is practice right?

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 var para = document.querySelector('p');
 var style = document.querySelector('style');
+var font  = document.querySelector('style #font');
 
 para.addEventListener('click', updateName);
 
-para.addEventListener('click', updateStyle)
+para.addEventListener('click', updateStyle);
+
+para.addEventListener('click', updateFont);
 
 function updateName() {
   var name = prompt('Enter a new name');
@@ -13,4 +16,11 @@ function updateName() {
 function updateStyle() {
   var color = prompt("red, blue, or yellow?");
   style.textContent = 'p {color: black; background:' + color + '}';
+}
+
+function updateFont() {
+  var font = prompt("like the font?")
+  if (font != "yes") {
+    font.textContent = 'p {font: monotype;}';
+  }
 }
